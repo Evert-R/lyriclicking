e58628b7d76fa282bf24a834594d070d37e64a69
Evert <evert.rot@gmx.com>
2020-09-27 23:57:40 +0200

    Created sounds app

--- collabo_writer/urls.py
+++ collabo_writer/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views import static
+from django.conf.urls.static import static
+from django.conf import settings
+from workspaces.views import work_space
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', work_space, name='work_space'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- sounds/models.py
+++ sounds/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class sound(models.Model):
+    """
+    Adding a sound to the database
+    """
+    title = models.CharField(max_length=50, default='')
+    audio = models.FileField(upload_to="sounds/")
+
+    def __str__(self):
+        return self.title
